6110905ccd4534ce365a8ba14bdff8b5213e8238
Nikolay <Horushko.Nikolay@gmail.com>
2015-05-21 16:06:36 +0300

    connection to data base

--- src/conection/ConnectionConfiguration.java
+++ src/conection/ConnectionConfiguration.java
@@ -1,9 +1,38 @@
 package conection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by Nikolay on 20.05.2015.
  */
 public class ConnectionConfiguration {
+          public static Connection getConnection(){
+            Connection connection=null;
 
-   //todo jdbc:mysql://localhost:3306/reportsBD
-}
+            try {
+
+                Class.forName("com.mysql.jdbc.Driver");
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/reportsBD", "root", "159357456");
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            return connection;
+        }
+
+    // Test connection to data base
+        public static void main(String[] args) {
+            Connection connection = null;
+
+            connection = ConnectionConfiguration.getConnection();
+            if (connection != null) System.out.println("estabilished");
+
+        }
+    }
+
+
---
+++ src/objects/Report.java
@@ -0,0 +1,7 @@
+package objects;
+
+/**
+ * Created by Nikolay on 21.05.2015.
+ */
+public class Report {
+}
